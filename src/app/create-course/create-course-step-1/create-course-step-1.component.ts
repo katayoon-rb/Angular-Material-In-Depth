1884fe559b43630c50c30d51448f2d3e11225490
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2024-05-06 17:29:22 +0330

    create course step 1

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { UntypedFormBuilder, Validators } from "@angular/forms";
+import { MatCalendarCellClassFunction } from "@angular/material/datepicker";
 
 @Component({
   selector: "create-course-step-1",
@@ -18,6 +19,14 @@ export class CreateCourseStep1Component {
     downloadsAllowed: [false, Validators.requiredTrue],
     longDescription: ["", [Validators.required, Validators.minLength(3)]],
   });
+
+  dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
+    const date = cellDate.getDate();
+    if (view == "month") {
+      return date == 1 ? "highlight-date" : "";
+    }
+    return "";
+  };
 
   constructor(private fb: UntypedFormBuilder) {}
 
